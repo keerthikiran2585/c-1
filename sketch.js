6934718ed5d790fb57399de5dfc2e12257644d2b
keerthikiran2585 <79265713+keerthikiran2585@users.noreply.github.com>
2021-06-01 13:29:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var hypnoticBall, database;
+var canvas;
+var gameState=0;
+var playerCount;
+var form, player, game;
+
+
+function setup(){
+  canvas=createCanvas(400, 400);
+  database = firebase.database();
+}
+
+function draw(){
+  background("white");
+  
+    
+  
+}
+
+
+  
